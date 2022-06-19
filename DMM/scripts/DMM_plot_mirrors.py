d2297ad2a60bf890a781f1d062c04696b1eb56b3
gianthk <gianthk.iori@gmail.com>
2022-06-19 22:31:50 +0300

    cleaned DMM folder

--- DMM/scripts/DMM_plot_mirrors.py
+++ DMM/scripts/DMM_plot_mirrors.py
@@ -8,7 +8,7 @@
 #   Author:         Gianluca Iori (gianthk.iori@gmail.com)
 #   SESAME - BEATS
 #   Created on:   10/04/2020
-#   Last update:  24/04/2020
+#   Last update:  28/04/2021
 #   ______________________________________________________
 
 # import pandas as pd
@@ -74,7 +74,8 @@ def M1_coordinates(l, theta):
 
 # variables definition      ##################################################################################
 fixed_exit = False # flag for fixed exit solution
-l = 0.38 # [m] mirror length
+l = 0.5 # [m] mirror length
+L_1_p = 510*1e-3 # [m] horizontal distance between the mirrors
 E_1 = 8e3 # [eV] lowest foreseen energy
 Y_1 = 11e-3 # [m] beam height @ first DMM mirror @ E_1
 E = float(input("Insert Energy [eV]: ")) # [eV] working energy
@@ -84,37 +85,12 @@ if E < E_1:
 
 lambda_1 = 1239.842/E_1 # nm
 lambda_E = 1239.842/E # nm
-d_spacing = float(input("Insert d-spacing [nm]: ")) # [nm]
 # d_spacing = 4.0 # [nm]
+d_spacing = float(input("Insert d-spacing [nm]: ")) # [nm]
 d_spacing_0 = 4.0 # [nm] d-spacing of the stripe with thicker layers
-
-o_0 = (input("Insert target OFFSET [mm] (leave empty for MIN. OFFSET): ")) # [mm]
-if o_0.isnumeric():
-    o_0 = float(o_0)*1e-3
-else:
-    o_0 = 0.0
 
 theta_1 = np.arcsin(lambda_1/(2*d_spacing_0)) # [rad] grazing angle at lowest foreseen energy
 theta = np.arcsin(lambda_E/(2*d_spacing)) # [rad] working grazing angle
-
-# OFFSET                    ##################################################################################
-if fixed_exit:
-    # check if the OFFSET given by user is < than Min. affordable one
-    if min_offset(l, theta_1, Y_1) > o_0:
-        o_0_p = min_offset(l, theta_1, Y_1)
-    else:
-        o_0_p = o_0
-else:
-    # only check if input OFFSET was left empty and if second mirror projects shadow on the first one!
-    o_min = min_offset(l, theta_1, Y_1)
-
-    if o_0 == 0.0:
-        o_0_p = o_min
-    else:
-        o_0_p = o_0
-    if (l / 2) * np.sin(theta) > o_0_p - (l / 2) * np.sin(theta):
-        print(f"warning! Given OFFSET is too small. Changed to Min. OFFSET: {o_min*1e3} [mm]")
-        o_0_p = o_min
 
 print(f"Min. Energy: {(E_1):.3} [eV]")
 print(f"Max. beam height @ entrance: {(1e3*Y_1):.3} [mm]")
@@ -128,13 +104,15 @@ print(f"Mirror 1 accepts {(1e3*l*np.sin(theta)):.3} mm")
 # FIRST MIRROR
 M1 = M1_coordinates(l, theta)
 
-# HORIZONTAL DISTANCE BETWEEN THE MIRRORS
-L_1_p = o_0_p/np.tan(2*theta) # [m]
+# OFFSET                    ##################################################################################
+o_0_p = L_1_p*np.tan(2*theta)  # [m]
 
 # SECOND MIRROR
 # M2 = np.array([[L_1_p-(l/2)*np.cos(theta), o_0_p-(l/2)*np.sin(theta)],
 #                [L_1_p, o_0_p],
 #                [L_1_p+(l/2)*np.cos(theta), o_0_p+(l/2)*np.sin(theta)]])
+# ML2 will have the same angle as ML1.
+# Its center position is translated by the distance between ML towers along X and by the DMM offset along Z.
 M2 = M1 + L_1_p*(np.array([[1,0],[1,0],[1,0]], dtype='float')) + o_0_p*(np.array([[0,1],[0,1],[0,1]], dtype='float'))
 
 # TRACE EXTREME RAYS        ##################################################################################
@@ -149,9 +127,9 @@ M1_ray = np.array([[-(footprint/2)*np.cos(theta), -(footprint/2)*np.sin(theta)],
 M2_ray = np.array([[L_1_p-(footprint/2)*np.cos(theta), o_0_p-(footprint/2)*np.sin(theta)],
                    [L_1_p+(footprint/2)*np.cos(theta), o_0_p+(footprint/2)*np.sin(theta)]])
 
-ray_low = np.array([[-0.5, M1_ray[0,0], M2_ray[0,0], M2_ray[0,0]+0.5],
+ray_low = np.array([[-1, M1_ray[0,0], M2_ray[0,0], M2_ray[0,0]+1],
                     [M1_ray[0,1], M1_ray[0,1], M2_ray[0,1], M2_ray[0,1]]])
-ray_high = np.array([[-0.5, M1_ray[1,0], M2_ray[1,0], M2_ray[0,0]+0.5],
+ray_high = np.array([[-1, M1_ray[1,0], M2_ray[1,0], M2_ray[0,0]+1],
                     [M1_ray[1,1], M1_ray[1,1], M2_ray[1,1], M2_ray[1,1]]])
 
 # PRINT OUTPUT              ##################################################################################
@@ -159,21 +137,25 @@ print(f"Energy: {E} [eV]")
 print(f"d-spacing: {d_spacing:.3} [nm]")
 print(f"Grazing angle: {rad2deg(theta):.3} [deg] ({(1e3*theta):.3} [mrad])")
 print(f"DMM OFFSET: {(o_0_p*1e3):.3} [mm]")
-print(f"Fixed exit: {fixed_exit}")
+# print(f"Fixed exit: {fixed_exit}")
 print(f"Mirrors distance: {L_1_p:.3} [m]")
+print(f"Mirrors length: {l*1e3:.3} [mm]")
 
 # PLOT DMM MIRRORS          ##################################################################################
 f, ax = plt.subplots()
-plt.plot(M1[:,0], M1[:,1], 'k', linewidth=4)
-plt.plot(M2[:,0], M2[:,1], 'k', linewidth=4)
-plt.plot(M1[1, 0], M1[1, 1], 'Xr',
-         M2[1,0], M2[1,1], 'Xr',
-         ray_low[0,:], ray_low[1,:], 'c',
-         ray_high[0,:], ray_high[1,:], 'c')
+plt.plot(M1[:,0], M1[:,1], 'k', linewidth=3)
+plt.plot(M2[:,0], M2[:,1], 'k', linewidth=3)
+plt.plot(M1[1, 0], M1[1, 1], 'Xr')
+plt.plot(M2[1,0], M2[1,1], 'Xr')
+plt.plot(ray_low[0,:], ray_low[1,:], 'c', linewidth=1)
+plt.plot(ray_high[0,:], ray_high[1,:], 'c', linewidth=1)
 
 plt.xlabel('Z [m]')
 plt.ylabel('Y [m]')
 plt.grid(True, which="both")
+plt.xticks(np.arange(-0.4, 1, 0.1))
+plt.yticks(np.arange(-0.005, 0.026, 0.001))
+ax.set(xlim=(-0.5, 1), ylim=(-0.005, 0.026))
 # ax.set_aspect('equal', 'box')
 plt.show()
 # f.savefig(f"DMM_mirrors_di{int(d_spacing_0)}_Ei{int(E_1/1e3)}_d{int(d_spacing)}_E{int(E/1e3)}_OFFSET{(o_0_p*1e3):.3}mm_Gr{rad2deg(theta):.3}deg_D{L_1_p:.3}m.png", bbox_inches='tight', dpi=600)
---
+++ DMM/scripts/README.md
@@ -0,0 +1,7 @@
+# BEATS Double Multilayer Monochromator 
+#### Scripts to reproduce plots of the DMM multilayers working positions.
+- See the folder [resources](resources) for a report containing the DMM specifications.
+- Plot the multilayers positions for a given working energy calling the script `DMM_plot_mirrors.py`:
+```commandline
+python scripts/DMM_plot_mirrors.py
+```
